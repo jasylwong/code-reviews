2c7f8b1c3d6dd6b6ba7bc3d9e4051171201b9f94
Jason <jasylwong@gmail.com>
2020-05-04 17:32:09 +0100

    pass test for empty string error

--- JavaScript/spell_checker.js
+++ JavaScript/spell_checker.js
@@ -1,5 +1,20 @@
 function spell_checker(sentence) {
-  return 'cat'
+  if (sentence === '') throw new Error('Empty string not allowed') 
+  
+  let word_bank = ['cat', 'music', 'the', 'dog']
+  words = sentence.split(' ')
+  output = []
+
+  words.forEach(word => {
+    if (word_bank.includes(word.toLowerCase())) {
+      output.push(word);
+    } else {
+      output.push(`~${word}~`);
+    }
+  }
+  )
+
+  return output.join(' ');
 }
 
 module.exports =  spell_checker;
--- JavaScript/spell_checker.test.js
+++ JavaScript/spell_checker.test.js
@@ -5,5 +5,43 @@ describe('spell checker', () => {
     test('three letter correct word returns same word', () => {
       expect(spell_checker('cat')).toEqual('cat')
     })
+
+    test('three letter incorrect word returned in between tildas', () => {
+      expect(spell_checker('cit')).toEqual('~cit~')
+    })
+
+    test('five letter correct word returns same word', () => {
+      expect(spell_checker('music')).toEqual('music')
+    })
+
+    test('five letter incorrect word returned in between tildas', () => {
+      expect(spell_checker('mxsic')).toEqual('~mxsic~')
+    })
+  })
+
+  describe('two words', () => {
+    test('two correct words returns same words', () => {
+      expect(spell_checker('the Dog')).toEqual('the Dog')
+    })
+
+    test('two incorrect words returns in between tildas', () => {
+      expect(spell_checker('ths dTg')).toEqual('~ths~ ~dTg~')
+    })
+  })
+
+  // describe('Full sentences with punctuation marks', () => {
+  //   test('Capitals and full stops', () => {
+  //     expect(spell_checker('Hello')).toEqual('the Dog')
+  //   })
+  // })
+
+  describe('edge cases', () => {
+    test('empty string throws an error', () => {
+      expect(() => {spell_checker('')}).toThrow('Empty string not allowed')
+    })
+    
+    test('empty string throws an error', () => {
+      expect(() => {spell_checker('')}).toThrow('Empty string not allowed')
+    })
   })
 })
